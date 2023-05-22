850772ecfa97ec423b0c72993ee07a01683077b8
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2023-05-22 16:36:44 +0000

    crud setup component

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
+export interface Todo {
+    id?: number;
+    todo?: string;
+    completed?: boolean;
+    userId?: number;
+}
+export type Todos = Todo[];
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  /*
+  * CRUD API for Todo
+  */
+  private _api = environment.todo.api;
+
+  async addTodo(todo: Todo): Promise<Todo> {
+    const response = await fetch(`${this._api}/add`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(todo),
+    });
+    return await response.json();
+  }
+
+
+  async getTodos(): Promise<Todos> {
+    const response = await fetch(`${this._api}`);
+    const { todos } = await response.json();
+    return todos;
+  }
+
+
+  async updateTodo(id: number, todo: Todo): Promise<Todo> {
+    const response = await fetch(`${this._api}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(todo),
+    });
+    return await response.json();
+  }
+
+  async deleteTodo(id: number): Promise<Todo> {
+    const response = await fetch(`${this._api}/${id}`, {
+      method: 'DELETE',
+    });
+    return await response.json();
+  }
 
   constructor() { }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo, TodoService, Todos } from '../services/todo.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss']
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
+  private _todos = new BehaviorSubject<Todos>([]);
+  readonly todos$ = this._todos.asObservable();
+  todo: string = '';
+  constructor(private _todoService: TodoService) { }
+  
+  ngOnInit(): void {
+    this._todoService.getTodos().then((todos) => {
+      this._todos.next(todos);
+    });
+  }
 
+  updateCompleted(event: any, todo: Todo) {
+    this.updateTodo({
+      ...todo,
+      completed: event.target.checked,
+    });
+  }
+
+  async addTodo(event: any) {
+    event.preventDefault();
+    if (this.todo.trim().length === 0) {
+      return;
+    }
+    
+    const todo = {
+      todo: this.todo,
+      completed: false,
+    };
+    this._todoService.addTodo(todo).then((todo) => {
+      this._todos.next([...this._todos.getValue(), todo]);
+      this.todo = '';
+    });
+  }
+
+  async updateTodo({id, completed}: Todo) {
+    if (!id) {
+      return;
+    }
+    this._todoService.updateTodo(id, {completed}).then((todo) => {
+      const todos = this._todos.getValue();
+      const index = todos.findIndex((t) => t.id === todo.id);
+      todos[index].completed = todo.completed;
+      this._todos.next(todos);
+    });
+  }
+
+  deleteTodo(id: number) {
+    this._todoService.deleteTodo(id).then((todo) => {
+      const todos = this._todos.getValue();
+      const index = todos.findIndex((t) => t.id === todo.id);
+      todos.splice(index, 1);
+      this._todos.next(todos);
+    });
+  }
 }
